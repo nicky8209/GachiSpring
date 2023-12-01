95f72c4338d7a2a7ddcb01066f273f2cd9c63075
nicky8209 <nicky8209@naver.com>
2023-12-01 15:15:16 +0900

    Add Item Create

---
+++ src/main/java/sample/spring/yse/ItemController.java
@@ -0,0 +1,15 @@
+package sample.spring.yse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ItemController {
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		return new ModelAndView("item/create");
+	}
+
+}
